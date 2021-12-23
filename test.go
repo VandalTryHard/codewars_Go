91c594dff42a06a663879c36033dfda29a471bbd
Gulenok Valentin <gulenok.valentin@yandex.ru>
2021-12-23 13:47:15 +0600

    solution codewars.com Go (aka Golang)
    8 kyu
    7 kyu

--- test.go
+++ test.go
@@ -2,24 +2,22 @@ package main
 
 import (
 	"fmt"
-	"strings"
+	"math"
 )
 
 func main() {
-	// PrinterError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz")
-	fmt.Println(PrinterError("aaaaaaaaaaaaaaaabbbbbbbbbbbbbbbbbbmmmmmmmmmmmmmmmmmmmxyz"))
-}
-
-func PrinterError(s string) string {
-	s := strings.Split(s, "")
-	// check := strings.Split("abcdefghijklm", "")
-	// col := 0
-	// for i, s := range s {
-	// 	if s == check {
-	// 		&col = *col + 1
-	// 	}
-	// }
-	// result = "%v/%v", col, len(s)
-	// result := strings.Join(s, ",")
-	return s
+	// kek := math.Ceil(52 / 100)
+	// kek := 52.0 / 100.0
+	// fmt.Println(math.Ceil(kek))
+	var l, w, h float64 = 6.3, 4.5, 3.29
+	roll := (52.0 / 100.0) * 10.0
+	fmt.Println(roll)
+	room := math.Ceil(((l + w) * h) * 2)
+	fmt.Println(room)
+	resultRoll := (room / roll) * 1.15
+	fmt.Println(resultRoll)
+	rolls := math.Ceil(resultRoll)
+	fmt.Println(rolls)
+	// result := numbers[rolls]
+	// fmt.Println(result)
 }
