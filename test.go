cbe5548bdf6045f0fbce268822963ada66b70be7
Gulenok Valentin <gulenok.valentin@yandex.ru>
2021-12-25 18:14:10 +0600

    solution codewars.com Go (aka Golang)
    8 kyu
    7 kyu

--- test.go
+++ test.go
@@ -2,22 +2,13 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	// kek := math.Ceil(52 / 100)
-	// kek := 52.0 / 100.0
-	// fmt.Println(math.Ceil(kek))
-	var l, w, h float64 = 6.3, 4.5, 3.29
-	roll := (52.0 / 100.0) * 10.0
-	fmt.Println(roll)
-	room := math.Ceil(((l + w) * h) * 2)
-	fmt.Println(room)
-	resultRoll := (room / roll) * 1.15
-	fmt.Println(resultRoll)
-	rolls := math.Ceil(resultRoll)
-	fmt.Println(rolls)
-	// result := numbers[rolls]
-	// fmt.Println(result)
+	fmt.Println(Potatoes(99, 100, 98))
+}
+
+func Potatoes(p0, w0, p1 int) int {
+	w1 := w0 * (100 - p0) / (100 - p1)
+	return w1
 }
